06cdc48ff842cfffa64d37e55029f23a0ce9da39
unverzed <vitoriarortega@gmail.com>
2022-10-24 17:17:17 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref, onMounted, computed, watch } from "vue";
+
+const todos = ref([]);
+const name = ref("");
+
+const input_content = ref("");
+const input_category = ref(null);
+const todos_asc = computed(() =>
+  todos.value.sort((a, b) => {
+    return a.createdAt - b.createdAt;
+  })
+);
+
+watch(name, (newValue) => {
+  localStorage.setItem("name", newValue);
+});
+
+onMounted(() => {
+  name.value = localStorage.getItem("name") || "";
+});
+</script>
+
+
+ <template>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" placeholder="Name Here" v-model="name" />
+      </h2>
+    </section>
+  </main>
+</template>
